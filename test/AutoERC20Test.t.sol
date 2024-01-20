a985c86ab45896f1fb6ef6e2547f3b328ec78ab6
Hebx <lord.noctx@gmail.com>
2024-01-20 05:50:41 +0100

    Added more tests

--- test/AutoERC20Test.t.sol
+++ test/AutoERC20Test.t.sol
@@ -51,4 +51,61 @@ contract AutoERC20Test is Test {
         vm.expectRevert();
         MintableToken(address(autoERC20)).mint(address(this), 10);
     }
+
+    function testTokenTransfer() public {
+        uint256 transferAmount = 100 ether;
+        uint256 bobInitialBalance = autoERC20.balanceOf(bob);
+
+        // Bob transfers tokens to Alice
+        vm.prank(bob);
+        autoERC20.transfer(alice, transferAmount);
+
+        assertEq(autoERC20.balanceOf(bob), bobInitialBalance - transferAmount);
+        assertEq(autoERC20.balanceOf(alice), transferAmount);
+        // assertEmittedCorrectTransferEvent(bob, alice, transferAmount);
+    }
+
+    function testTransferFailInsufficientBalance() public {
+        uint256 bobBalance = autoERC20.balanceOf(bob);
+        uint256 transferAmount = bobBalance + 1 ether; // more than Bob has
+
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+        vm.prank(bob);
+        autoERC20.transfer(alice, transferAmount);
+    }
+
+    function testCorrectOwnerBalance() public {
+        assertEq(
+            autoERC20.balanceOf(deployerAddress),
+            deployerAutoERC20.INITIAL_SUPPLY() - BOB_STARTING_AMOUNT
+        );
+    }
+
+    function testTransferFromExceedingAllowance() public {
+        uint256 initialAllowance = 1000;
+        uint256 transferAmount = 1500; // more than the allowance
+
+        // Alice approves bob to spend tokens on her behalf
+        vm.prank(alice);
+        autoERC20.approve(bob, initialAllowance);
+
+        vm.expectRevert("ERC20: insufficient allowance");
+        vm.prank(bob);
+        autoERC20.transferFrom(alice, bob, transferAmount);
+    }
+
+    // function assertEmittedCorrectTransferEvent(
+    //     address from,
+    //     address to,
+    //     uint256 value
+    // ) internal {
+    //     vm.expectEmit(true, true, true, true);
+    //     emit Transfer(from, to, value);
+    // }
+
+    function testApproveFromZeroAddress() public {
+        vm.expectRevert("ERC20: approve from the zero address");
+        vm.prank(address(0));
+        autoERC20.approve(alice, 1000);
+    }
 }
